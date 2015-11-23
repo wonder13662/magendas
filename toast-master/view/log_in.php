dc05cba1472a3a1fa9bb54f238a881736ec6d367
wonder13662 <wonder13662@gmail.com>
2015-11-23 16:29:50 +0900

    Email login is enabled!
    Email login is enabled!
    Still maintaining phonen num is available.
    But it will be deprecated soon!

--- toast-master/view/log_in.php
+++ toast-master/view/log_in.php
@@ -65,11 +65,119 @@ var meeting_membership_id = <?php echo json_encode($meeting_membership_id);?>;
 
 console.log(">>> meeting_membership_id :: ",meeting_membership_id);
 
+var delegate_on_log_in = 
+_obj.get_delegate(
+	// delegate_func
+	function(data){
+
+		// console.log(">>> log in / success / data : ",data);
+		// console.log(">>> log in / success / redirect_url : ",redirect_url);
+
+		if(data == null || data.member_info == null) {
+
+	    	// 2. 로그인 실패.
+	    	// 2-1. 실패 메시지 노출
+	    	// 2-2. ID / PASSWORD focusing - 지우지는 않습니다.
+
+	    	var message_jq = $("div#message"); 
+	    	message_jq.html("ID or Password is not valid!");
+	    	message_jq.show();
+
+		} else {
+
+	    	// 1. 로그인 성공.
+	    	// 1-1. 로그인에 성공한 멤버 아이디를 쿠키에 등록합니다.
+	    	_server.setCookie("tm_login_user",data.member_info.__member_id, 1);
+	    	
+	    	var membership_arr_json_str = data.query_output_arr[4];
+	    	var membership_arr = $.parseJSON(membership_arr_json_str);
+
+	    	if(membership_arr == undefined || membership_arr.length < 1) {
+
+	    		alert("Membership data is not correct\n\n[Error-511]");
+
+	    	} else if (1 < membership_arr.length) {
+
+	    		// 1-2. 해당 유저의 가입 클럽이 2개 이상일 경우, 클럽 선택 페이지로 이동합니다.
+				_link.go_there(_link.MEMBERSHIP_PICKER);
+
+			} else if (1 == membership_arr.length) {
+
+				// 1-3. 해당 유저의 가입 클럽이 1개일 경우는 해당 클럽의 메인 페이지로 리다이렉트합니다.
+				var cur_membership = membership_arr[0];
+				var __member_membership = parseInt(cur_membership.__member_membership);
+
+				_link.go_there(
+					_link.MEETING_AGENDA
+					, _param.get(_param.MEETING_MEMBERSHIP_ID,__member_membership)
+				);
+
+	    	}
+
+		} // end if
+
+	},
+	// delegate_scope
+	this
+)
+
+
 $("button#log_in_btn").on("click", function(e){
 
 	e.preventDefault();
-	
+
+	var password_jq = $("input#password");
+	var password = password_jq.val();
+	if(_v.isNotValidStr(password)){
+		alert("Password is empty!");
+		password_jq.focus();
+	}
+
+	// TODO 
 	var mobile_number_jq = $("input#mobile_number");
+
+	// 1. 이메일 주소로 멤버 여부를 확인.
+	var email_address = mobile_number_jq.val();
+
+	var regex_pattern_email_address = /[0-9a-zA-Z][_0-9a-zA-Z-]*@[_0-9a-zA-Z-]+(\.[_0-9a-zA-Z-]+){1,2}$/;
+	var regex_pattern_email_address_match = email_address.match(regex_pattern_email_address);
+
+	// does it works?
+	if (_v.isValidArray(regex_pattern_email_address_match)) {
+
+	    // invalid emailaddress
+		var request_param_obj = 
+		_param
+		.get(_param.MEMBER_EMAIL, email_address)
+		.get(_param.MEMBER_PASSWORD, password)
+		.get(_param.MEETING_MEMBERSHIP_ID, meeting_membership_id)
+		;
+
+		_ajax.send_simple_post(
+			// _url
+			_link.get_link(_link.API_LOG_IN)
+			// _param_obj
+			,request_param_obj
+			// _delegate_after_job_done
+			,_obj.get_delegate(
+				// delegate_func
+				function(data){
+
+					delegate_on_log_in._func.apply(delegate_on_log_in._scope,[data]);
+
+				},
+				// delegate_scope
+				this
+			)
+		); // ajax done.
+
+		return;
+	}
+
+
+
+
+	// 2. 전화번호로 멤버 여부를 확인. (Deprecated)
 	var mobile_number = mobile_number_jq.val();
 
 	if(_v.isNotValidStr(mobile_number)){
@@ -96,13 +204,6 @@ $("button#log_in_btn").on("click", function(e){
 		mobile_number = formatted_mobile_number;
 	}
 
-	var password_jq = $("input#password");
-	var password = password_jq.val();
-	if(_v.isNotValidStr(password)){
-		alert("Password is empty!");
-		password_jq.focus();
-	}
-
 	var request_params = 
 	_param
 	.get(_param.MEMBER_MOBILE, mobile_number)
@@ -122,54 +223,8 @@ $("button#log_in_btn").on("click", function(e){
         data:request_params,
         success:function(data){
 
-        	console.log(">>> log in / success / data : ",data);
-        	console.log(">>> log in / success / redirect_url : ",redirect_url);
+        	delegate_on_log_in._func.apply(delegate_on_log_in._scope,[data]);
 
-        	if(data == null || data.member_info == null) {
-
-            	// 2. 로그인 실패.
-            	// 2-1. 실패 메시지 노출
-            	// 2-2. ID / PASSWORD focusing - 지우지는 않습니다.
-
-            	var message_jq = $("div#message"); 
-            	message_jq.html("ID or Password is not valid!");
-            	message_jq.show();
-
-        	} else {
-
-            	// 1. 로그인 성공.
-            	// 1-1. 로그인에 성공한 멤버 아이디를 쿠키에 등록합니다.
-            	_server.setCookie("tm_login_user",data.member_info.__member_id, 1);
-            	
-            	var membership_arr_json_str = data.query_output_arr[4];
-            	var membership_arr = $.parseJSON(membership_arr_json_str);
-
-            	if(membership_arr == undefined || membership_arr.length < 1) {
-
-            		alert("Membership data is not correct\n\n[Error-511]");
-
-            	} else if (1 < membership_arr.length) {
-
-            		console.log("001");
-
-            		// 1-2. 해당 유저의 가입 클럽이 2개 이상일 경우, 클럽 선택 페이지로 이동합니다.
-					_link.go_there(_link.MEMBERSHIP_PICKER);
-
-        		} else if (1 == membership_arr.length) {
-
-        			console.log("002");
-
-        			// 1-3. 해당 유저의 가입 클럽이 1개일 경우는 해당 클럽의 메인 페이지로 리다이렉트합니다.
-        			var cur_membership = membership_arr[0];
-        			var __member_membership = parseInt(cur_membership.__member_membership);
-
-					_link.go_there(
-						_link.MEETING_AGENDA
-						, _param.get(_param.MEETING_MEMBERSHIP_ID,__member_membership)
-					);
-
-            	}
-        	}
         }
     }).fail(function( data ) {
     	console.log("fail / data.responseText : ",data.responseText);
