56fc61cbb69175ca637751bfa59d728288dba8e1
wonder13662 <wonder13662@gmail.com>
2016-06-05 07:57:16 +0900

    action file feature added.
    1. action file is stored and retrieve by zipped file.

--- toast-master/view/action_test.php
+++ toast-master/view/action_test.php
@@ -3,21 +3,21 @@
 	// @ common setting
 	include_once("../common.inc");
 
-	// 1. php action template을 가져옵니다.
-	// 1-1. 각 엘리먼트의 id가 부여되어야 합니다. id는 숫자가 아닌 hashkey로 처리됩니다.
+	// TEST - SAVE
+	// $action_obj = ActionTemplate::get_TM_Default("07:30", "Special Meeting");
+	// $cur_hash_key = $action_obj->get_hash_key();
+	// echo "\$cur_hash_key :: $cur_hash_key<br/>";
+	// ActionFileManager::save("2016-06-03", $action_obj);
 
-	// 2. php action template을 json string으로 바꿉니다. 
+	// TEST - LOAD
+	$target_YYYYMMDD = "2016-06-03";
+	// $target_hash_key = "2b5033aaa9b7dc03a4138f4082e4cee9"; // OFFICER TABLE
+	// $target_hash_key = "7e31adfeaed978868c3b1f0a93515840";	// MEETING TABLE
+	$target_hash_key = "cad323cbd4f831b5cf18c47c0e50c992";	// MEETING TABLE ZIPPED
 
-	// 3. php action template을 파일로 저장합니다.
+	// 
 
-	// 4. 저장한 파일을 로딩, json string을 불러옵니다. (검사의 목적이 있습니다.)
-
-	// 5. json string을 view의 javascript로 넘겨줍니다.
-
-
-	// 사용자가 action을 업데이트 할 경우!
-	// 1. 
-
+	$content = ActionFileManager::load($target_YYYYMMDD, $target_hash_key);
 
 
 	// @ required
@@ -45,10 +45,10 @@
 
 <script>
 
-console.log("HERE / 001 - XXX");
-
 // php to javascript sample
-// var meeting_agenda_list = <?php echo json_encode($meeting_agenda_list);?>;
+var content = <?php echo json_encode($content);?>;
+var content_obj = $.parseJSON(content);
+console.log("content_obj ::: ",content_obj);
 
 
 
